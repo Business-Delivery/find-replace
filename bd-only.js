764b1a13e589fea095bc45af355067afb7d63b86
KevDoy <KevDoy@users.noreply.github.com>
2018-05-01 12:13:54 -0700

    push to web

--- bd-only.js
+++ bd-only.js
@@ -1,3 +1,3 @@
 // JavaScript Document
-
+// Maintained by BD
 $( "p.bse-hero-description:first-of-type" ).replaceWith( "<p class='bse-hero-description'>Save even more on the products you use every day.</p>" );
